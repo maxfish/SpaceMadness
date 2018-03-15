71397f848d769ae0df3155af479cccb5d7eadd80
maksym <maksym@yelp.com>
2018-03-15 12:56:58 +0100

    shield moves following an ellipse

--- game/shield.py
+++ game/shield.py
@@ -6,29 +6,29 @@ from mgl2d.math.vector2 import Vector2
 
 class Shield(Entity):
     def __init__(self, ship):
-        super().__init__(
-            ship._world,
-            ship._position.x,
-            ship._position.y,
-            ship._z,
-        )
+        super().__init__(ship._world, 0, 0)
 
         self._ship = ship
+        self._position = self._ship.position
+        self._angle = 0
+        self._rad1 = 200
+        self._rad2 = 400
+
         self._quad = QuadDrawable(0, 0, 255, 255)
-        self._quad.pos = self.position
+        self._quad.pos = self._position
         self._quad.texture = Texture.load_from_file('resources/images/shield_arc.png')
         self._quad.anchor = Vector2(127,127)
 
-        self._radius = 200
-
     def update(self, game_speed):
-        self._quad.angle += 0.1
+        self._angle += 0.4
         self._position = \
             self._ship._position + \
             Vector2(
-                self._radius * math.cos(math.radians(self._quad.angle)),
-                self._radius * math.sin(math.radians(self._quad.angle)),
+                self._rad1 * math.cos(math.radians(self._angle-45)),
+                self._rad2 * math.sin(math.radians(self._angle-45)),
             )
+
+        self._quad.angle = self._angle
         self._quad.pos = self.position
 
     def draw(self, screen):
