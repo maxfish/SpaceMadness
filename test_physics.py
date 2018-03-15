be1b95f6def624014896d00fd0dbec5c53ed9ead
Max Pesce <mpesce@yelp.com>
2018-03-15 15:23:22 +0100

    ship

--- test_physics.py
+++ test_physics.py
@@ -2,6 +2,8 @@
 import sys
 
 from Box2D import b2World, b2FixtureDef, b2BodyDef
+from mgl2d.app import App
+from mgl2d.input.game_controller_manager import GameControllerManager
 from random import randint
 import sdl2
 import sdl2.ext
@@ -12,8 +14,10 @@ from physics.physic_ship import PhysicShip
 GAME_FPS = 50
 GAME_FRAME_MS = 1000 / GAME_FPS
 
+app = App()
 
-def draw_line(surface, x1,y1,x2,y2):
+
+def draw_line(surface, x1, y1, x2, y2):
     color = sdl2.ext.Color(255, 255, 255)
     sdl2.ext.line(surface, color, (x1, y1, x2, y2))
 
@@ -31,7 +35,7 @@ def draw_rects(surface, width, height):
 
 
 physicsWorld = b2World(gravity=(0, 0))
-pShip = PhysicShip(physicsWorld, 10, 10)
+pShip = PhysicShip(physicsWorld, 10, 30)
 
 timeStep = 1.0 / GAME_FPS
 vel_iters, pos_iters = 6, 2
@@ -42,14 +46,22 @@ window.show()
 
 windowsurface = window.get_surface()
 
+
 def draw_polygon(screen, body, polygon):
-    vertices = [(body.transform * v) * 5 for v in polygon.vertices]
-    vertices = [(v[0], 500 - v[1]) for v in vertices]
+    vertices = [(body.transform * v) * 10 for v in polygon.vertices]
+    vertices = [(v[0], 1080 - v[1]) for v in vertices]
     for i in range(0, len(vertices)):
-        draw_line(screen, int(vertices[i][0]), int(vertices[i][1]), int(vertices[(i+1)%len(vertices)][0]), int(vertices[(i+1)%len(vertices)][1]))
+        draw_line(screen, int(vertices[i][0]), int(vertices[i][1]), int(vertices[(i + 1) % len(vertices)][0]),
+                  int(vertices[(i + 1) % len(vertices)][1]))
 
 
 # b2PolygonShape.draw = my_draw_polygon
+controllerManager = GameControllerManager()
+controllerManager.load_joysticks_database('resources/gamecontrollerdb.txt')
+controller = controllerManager.grab_controller()
+
+timeStep = 1.0 / GAME_FPS
+vel_iters, pos_iters = 6, 2
 
 
 def run():
@@ -60,7 +72,10 @@ def run():
             if event.type == sdl2.SDL_QUIT:
                 running = False
                 break
-
+        sdl2.ext.fill(windowsurface, 0)
+        physicsWorld.Step(timeStep, vel_iters, pos_iters)
+        physicsWorld.ClearForces()
+        pShip.update_forces(controller)
         for fixture in pShip.body.fixtures:
             draw_polygon(windowsurface, pShip.body, fixture.shape)
         window.refresh()
