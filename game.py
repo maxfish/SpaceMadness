02226281db3ddb8b0c3f65c816f9588c49c346d1
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2018-03-17 16:56:05 +0100

    Fullscreen using MOGL2D 0.1.6

--- game.py
+++ game.py
@@ -2,13 +2,12 @@
 import argparse
 import logging
 
-import sdl2
-import sdl2.ext as sdl2ext
 from mgl2d.app import App
 from mgl2d.graphics.screen import Screen
 from mgl2d.input.game_controller_manager import GameControllerManager
 
-from config import GAME_FPS, GAME_FRAME_MS, SCREEN_HEIGHT, SCREEN_WIDTH
+from config import GAME_FPS, GAME_FRAME_MS, SCREEN_HEIGHT, SCREEN_WIDTH, PHYSICS_TIME_STEP, PHYSICS_NUM_POS_ITERS, \
+    PHYSICS_NUM_VEL_ITERS
 from game.stages.stage_sky import StageSky
 from game.world import World
 
@@ -23,11 +22,6 @@ args = parser.parse_args()
 
 app = App()
 
-screen = Screen(args.width, args.height, 'Space Madness')
-if args.fullscreen:
-    sdl2.SDL_SetWindowFullscreen(screen._window, True)
-screen.print_info()
-
 controllerManager = GameControllerManager()
 controllerManager.load_joysticks_database('resources/gamecontrollerdb.txt')
 # debug: add keyboard
@@ -37,27 +31,16 @@ controllers = [
     for n in range(num_joysticks)
 ]
 
+screen = Screen(args.width, args.height, 'Space Madness')
+if args.fullscreen:
+    screen.full_screen = True
+screen.print_info()
+
 world = World(
     bounds=screen.viewport,
     controllers=controllers,
     stage=StageSky(screen.width, screen.height)
 )
-
-# ppe = PostProcessingStep(screen.width, screen.height)
-# ppe.drawable.shader = Shader.from_files('resources/shaders/base.vert', 'resources/shaders/postprocessing_retro.frag')
-# screen.add_postprocessing_step(ppe)
-
-
-timeStep = (1.0 / GAME_FPS) * 4
-vel_iters, pos_iters = 6, 2
-
-global prev_deletion
-global deletion_freq
-
-
-def draw_line(surface, x1, y1, x2, y2):
-    color = sdl2.ext.Color(255, 255, 255)
-    sdl2.ext.line(surface, color, (x1, y1, x2, y2))
 
 
 def draw_frame(screen):
@@ -71,7 +54,7 @@ def update_frame(delta_ms):
     world.update(delta_ms / GAME_FRAME_MS)
     for p in world.players:
         p.handle_input()
-    world.physicsWorld.Step(timeStep, vel_iters, pos_iters)
+    world.physicsWorld.Step(PHYSICS_TIME_STEP, PHYSICS_NUM_VEL_ITERS, PHYSICS_NUM_POS_ITERS)
 
 
 app.run(screen, draw_frame, update_frame, fps=GAME_FPS)
